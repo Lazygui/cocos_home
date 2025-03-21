82c418b39f5801556a7a5a6e00a5fdbf94148012
zhouzhijun <15642532452@163.com>
2025-03-21 13:33:36 +0800

    refactor(homepage): 优化角度测试功能
    - 修改鼠标移动事件监听对象，提高代码灵活性
    - 优化节点坐标计算逻辑，提升代码可读性
    - 调整场景文件结构，可能影响节点数量或属性

--- assets/homepage/angle.ts
+++ assets/homepage/angle.ts
@@ -3,19 +3,24 @@ const { ccclass, property } = _decorator;
 
 @ccclass("angle")
 export class angle extends Component {
+       //节点坐标倍数
        private move = {
               textNode: 0.08,
               iconNode: 0.05,
               bgNode: 0.02
        };
        start() {
-              const parent = this.node.parent;
-              parent.on(NodeEventType.MOUSE_MOVE, (event: EventMouse) => {
+              //监听画布节点
+              this.node.on(NodeEventType.MOUSE_MOVE, (event: EventMouse) => {
+                     //获取节点坐标
                      const mouseDeltaX = event.getDeltaX();
                      const mouseDeltaY = event.getDeltaY();
+                     //获取需要移动节点组
                      const right = event.target._children.filter((item: any): boolean => item._name === "right")[0];
                      if (right) {
+                            //获取需要移动节点组
                             const moveChildren = right._children.filter((item: any): boolean => item._name.includes("Node"));
+                            //设置节点坐标
                             for (let i = 0; i < moveChildren.length; i++) {
                                    const item = moveChildren[i];
                                    const itemPosition = item.getPosition();
